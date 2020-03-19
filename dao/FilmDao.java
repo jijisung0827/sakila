711c98ad93decf9192ebc3902882fe3bf4618b2a
jijisung0827 <62370179+jijisung0827@users.noreply.github.com>
2020-03-19 15:54:08 +0900

    Update FilmDao.java

--- dao/FilmDao.java
+++ dao/FilmDao.java
@@ -24,3 +24,89 @@ public class FilmDao {
 		return list;
 	}
 }
+
+---------------------------------------------------------------------------------------------------------------------------------
+FilmActorDao.java
+
+package dao;
+
+import java.sql.*;
+import java.util.*;
+import util.*;
+import vo.*;
+
+public class FilmActorDao {
+	public ArrayList<FilmActor> selectfilmActor() throws Exception{
+		DbUtil dbUtil = new DbUtil();
+		Connection conn = dbUtil.getConnection();
+		String sql = "SELECT * FROM film_actor";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		ArrayList<FilmActor> list = new ArrayList<FilmActor>();
+		while(rs.next()) {
+			FilmActor filmActor = new FilmActor();
+			filmActor.setActorId(rs.getInt("actor_id"));
+			filmActor.setFilmId(rs.getInt("film_id"));
+			filmActor.setLastUpdate(rs.getString("last_update"));
+			list.add(filmActor);
+		}
+		return list;
+	}
+}
+
+----------------------------------------------------------------------------------------------------------------------------------
+FilmCategoryDao.java
+
+package dao;
+import java.sql.*;
+import java.util.*;
+import util.*;
+import vo.*;
+
+public class FilmCategoryDao {
+	public ArrayList<FilmCategory> selectfilmCategory() throws Exception{
+		DbUtil dbUtil = new DbUtil();
+		Connection conn = dbUtil.getConnection();
+		String sql = "SELECT * FROM film_category";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		ArrayList<FilmCategory> list = new ArrayList<FilmCategory>();
+		while(rs.next()) {
+			FilmCategory filmCategory = new FilmCategory();
+			filmCategory.setCategoryId(rs.getInt("category_id"));
+			filmCategory.setFilmId(rs.getInt("film_id"));
+			filmCategory.setLastUpdate(rs.getString("last_update"));
+			list.add(filmCategory);
+		}
+		return list;
+	}
+}
+
+--------------------------------------------------------------------------------------------------------------------
+FilmTextDao.java
+
+package dao;
+import java.sql.*;
+import java.util.*;
+import util.*;
+import vo.*;
+
+public class FilmTextDao {
+	public ArrayList<FilmText> selectfilmText() throws Exception{
+		DbUtil dbUtil = new DbUtil();
+		Connection conn = dbUtil.getConnection();
+		String sql = "SELECT * FROM film_text";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		ArrayList<FilmText> list = new ArrayList<FilmText>();
+		while(rs.next()) {
+			FilmText filmText = new FilmText();
+			filmText.setDescription(rs.getString("description"));
+			filmText.setFilmId(rs.getInt("film_id"));
+			filmText.setTitle(rs.getString("title"));
+			list.add(filmText);
+		}
+		return list;
+	}
+}
+
